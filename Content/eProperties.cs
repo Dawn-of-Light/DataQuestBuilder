b96da1b151330a7169075ee4ea3446a7b7554601
OntheDOL <OntheDOL@users.noreply.github.com>
2017-04-27 19:51:19 -0700

    fix for allowed class selection
    Converts class name to the numerical form required, and serializes them.

--- Content/eProperties.cs
+++ Content/eProperties.cs
@@ -43,5 +43,62 @@ namespace DataQuestBuilder
 			collectFinish = 11		// Player must give the target an item to finish the quest
             //Unknown = 255
         }
+        /// <summary>        
+        /// All the available classes.
+        /// </summary>
+        public enum eCharacterClass : byte
+        {
+        	Armsman = 2,
+			Cabalist = 13,
+			Cleric = 6,
+			Friar = 10,
+			Heretic = 33,
+			Infiltrator = 9,
+			Mercenary = 11,
+			Minstrel = 4,
+			Necromancer = 12,
+			Paladin = 1,
+			Reaver = 19,
+			Scout = 3,
+			Sorcerer = 8,
+			Theurgist = 5,
+			Wizard = 7,
+			MaulerAlb = 60,
+	
+			//mid classes
+			Berserker = 31,
+			Bonedancer = 30,
+			Healer = 26,
+			Hunter = 25,
+			Runemaster = 29,
+			Savage = 32,
+			Shadowblade = 23,
+			Shaman = 28,
+			Skald = 24,
+			Spiritmaster = 27,
+			Thane = 21,
+			Valkyrie = 34,
+			Warlock = 59,
+			Warrior = 22,
+			MaulerMid = 61,
+	
+			//hib classes
+			Animist = 55,
+			Bainshee = 39,
+			Bard = 48,
+			Blademaster = 43,
+			Champion = 45,
+			Druid = 47,
+			Eldritch = 40,
+			Enchanter = 41,
+			Hero = 44,
+			Mentalist = 42,
+			Nightshade = 49,
+			Ranger = 50,
+			Valewalker = 56,
+			Vampiir = 58,
+			Warden = 46,
+			MaulerHib = 62
+        }
     }
 }
---
+++ Controls/eCharacterClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DataQuestBuilder
+{
+	public class eCharacterClass : ListBox
+	{
+		public void Load()
+        {
+            Items.Clear();
+
+            foreach (string type in Enum.GetNames(typeof(eProperties.eCharacterClass)))
+            {
+                Items.Add(type);
+            }
+        }
+	}
+}
